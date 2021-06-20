805146d3b66df7f9f299ba456daf8a24e1617495
Waylon Dixon <wayjay4@yahoo.com>
2021-06-19 21:10:22 -0700

    added reqeust to params

--- app/Http/Controllers/AuthapiController.php
+++ app/Http/Controllers/AuthapiController.php
@@ -14,7 +14,7 @@ class AuthapiController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
         // set validation rules for request fields
         $rules = [
